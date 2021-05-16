febc150dc76baa78cc0a493e4881454f4d2c3b32
msiglreith <m.siglreith@gmail.com>
2021-05-16 14:00:37 +0200

    Add `vector` intrinsic type attribute
    Compared to the repr(simd) layout the vector types the ones generated with `vector` attribute will follow the scalar layout.

--- crates/rustc_codegen_spirv/src/spirv_type.rs
+++ crates/rustc_codegen_spirv/src/spirv_type.rs
@@ -350,9 +350,7 @@ impl SpirvType {
             Self::Bool => Size::from_bytes(1),
             Self::Integer(width, _) | Self::Float(width) => Size::from_bits(width),
             Self::Adt { size, .. } => size?,
-            Self::Vector { element, count } => {
-                cx.lookup_type(element).sizeof(cx)? * count.next_power_of_two() as u64
-            }
+            Self::Vector { element, count } => cx.lookup_type(element).sizeof(cx)? * count as u64,
             Self::Matrix { element, count } => cx.lookup_type(element).sizeof(cx)? * count as u64,
             Self::Array { element, count } => {
                 cx.lookup_type(element).sizeof(cx)? * cx.builder.lookup_const_u64(count).unwrap()
@@ -377,13 +375,7 @@ impl SpirvType {
             Self::Bool => Align::from_bytes(1).unwrap(),
             Self::Integer(width, _) | Self::Float(width) => Align::from_bits(width as u64).unwrap(),
             Self::Adt { align, .. } => align,
-            // Vectors have size==align
-            Self::Vector { .. } => Align::from_bytes(
-                self.sizeof(cx)
-                    .expect("alignof: Vectors must be sized")
-                    .bytes(),
-            )
-            .expect("alignof: Vectors must have power-of-2 size"),
+            Self::Vector { element, .. } => cx.lookup_type(element).alignof(cx),
             Self::Array { element, .. }
             | Self::RuntimeArray { element }
             | Self::Matrix { element, .. } => cx.lookup_type(element).alignof(cx),
