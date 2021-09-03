1864efe1c7cc4d4fbe81bac34b534570eb99d607
msiglreith <m.siglreith@gmail.com>
2021-09-03 20:30:03 +0200

    resource access through pointer types only

--- crates/rustc_codegen_spirv/src/builder/resource_access.rs
+++ crates/rustc_codegen_spirv/src/builder/resource_access.rs
@@ -18,55 +18,31 @@ impl<'a, 'tcx> Builder<'a, 'tcx> {
         let bindless_idx = args[0].def(self);
 
         match self.lookup_type(result_type) {
-            SpirvType::Sampler => {
-                let result_ptr = SpirvType::Pointer {
-                    pointee: result_type,
-                }
-                .def(rustc_span::DUMMY_SP, self);
-
-                let set = self.cx.sampler_bindless_descriptor_set();
-                let access = self
-                    .emit()
-                    .access_chain(result_ptr, None, set, vec![bindless_idx])
-                    .unwrap();
-                self.emit()
-                    .load(result_type, None, access, None, std::iter::empty())
-                    .unwrap()
-                    .with_type(result_type)
-            }
-            SpirvType::Image { dim, sampled, .. } => {
-                let result_ptr = SpirvType::Pointer {
-                    pointee: result_type,
-                }
-                .def(rustc_span::DUMMY_SP, self);
-
-                let set = self.cx.texture_bindless_descriptor_set(result_type, dim, sampled);
-                let access = self
-                    .emit()
-                    .access_chain(result_ptr, None, set, vec![bindless_idx])
-                    .unwrap();
-                self.emit()
-                    .load(result_type, None, access, None, std::iter::empty())
-                    .unwrap()
-                    .with_type(result_type)
-            }
-            SpirvType::AccelerationStructureKhr => {
-                let result_ptr = SpirvType::Pointer {
-                    pointee: result_type,
-                }
-                .def(rustc_span::DUMMY_SP, self);
-
-                let set = self.cx.acceleration_structure_bindless_descriptor_set();
-                let access = self
-                    .emit()
-                    .access_chain(result_ptr, None, set, vec![bindless_idx])
-                    .unwrap();
-                self.emit()
-                    .load(result_type, None, access, None, std::iter::empty())
-                    .unwrap()
-                    .with_type(result_type)
-            }
             SpirvType::Pointer { pointee } => match self.lookup_type(pointee) {
+                SpirvType::Sampler => {
+                    let set = self.cx.sampler_bindless_descriptor_set();
+                    self
+                        .emit()
+                        .access_chain(result_type, None, set, vec![bindless_idx])
+                        .unwrap()
+                        .with_type(result_type)
+                }
+                SpirvType::Image { dim, sampled, .. } => {
+                    let set = self.cx.texture_bindless_descriptor_set(pointee, dim, sampled);
+                    self
+                        .emit()
+                        .access_chain(result_type, None, set, vec![bindless_idx])
+                        .unwrap()
+                        .with_type(result_type)
+                }
+                SpirvType::AccelerationStructureKhr => {
+                    let set = self.cx.acceleration_structure_bindless_descriptor_set();
+                    self
+                        .emit()
+                        .access_chain(result_type, None, set, vec![bindless_idx])
+                        .unwrap()
+                        .with_type(result_type)
+                }
                 SpirvType::RuntimeArray { .. } => {
                     let uint_ty = SpirvType::Integer(32, false).def(rustc_span::DUMMY_SP, self);
                     let zero = self.constant_int(uint_ty, 0).def(self);
@@ -82,7 +58,7 @@ impl<'a, 'tcx> Builder<'a, 'tcx> {
                     ty
                 )),
             },
-            ty => self.fatal(&format!("{:?} unsupported for resource access", ty)),
+            ty => self.fatal(&format!("Non-Pointer type {:?} unsupported for resource access", ty)),
         }
     }
 }
